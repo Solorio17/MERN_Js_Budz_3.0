2a314e29ea6832885891f727fa7b415fd046b956
Jason Solorio <jasonsoloriom@gmail.com>
2018-11-01 16:24:01 -0600

    Creating routes for each component, and linking them together. And using the switch router to create a fallback route for an invalid route.

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import About from './components/about';
-import Home from './components/home';
+import { BrowserRouter as Router, Switch} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 import ApolloClient from 'apollo-boost';
 import {ApolloProvider} from 'react-apollo';
+
+import Home from './components/home';
+import Budz from './components/budz';
+import Dabz from './components/dabz';
+import Ediblez from './components/ediblez';
+import Drinkz from './components/drinkz';
+
 
 const client = new ApolloClient({
   uri: "http://localhost:4000"
@@ -12,12 +19,41 @@ const client = new ApolloClient({
  class App extends Component {
    render(){
      return (
-       <div>
-         <Home/>
-       </div>
+       <Router>
+         <Switch>
+           <Route path="/" exact render={
+               ()=>{
+                 return (<Home /> );
+               }
+             }/>
+           <Route path="/budz" exact render={
+               ()=>{
+                 return (<Budz /> );
+               }
+             }/>
+           <Route path="/dabz" exact render={
+               ()=>{
+                 return (<Dabz /> );
+               }
+             }/>
+             <Route path="/ediblez" exact render={
+                 ()=>{
+                   return (<Ediblez /> );
+                 }
+               }/>
+           <Route path="/drinkz" exact render={
+               ()=>{
+                 return (<Drinkz /> );
+               }
+             }/>
+
+           <Route render={() => { return (<h3>Route not found!</h3>)}}/>
+         </Switch>
+       </Router>
      );
    }
  }
+
  ReactDOM.render(
    <ApolloProvider client={client}>
      <App/>
