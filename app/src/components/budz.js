6e8447e1ca67a82d906203f9ae03f2a86946b204
Jason Solorio <jasonsoloriom@gmail.com>
2018-11-02 14:46:57 -0600

    Moving the budList display into budz.js from about.js

--- app/src/components/budz.js
+++ app/src/components/budz.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-const Budz = () =>{
-  return(
-    <h1>Hello from Budz</h1>
-  )
+const BudQuery = gql`
+{
+  budList{
+    strain
+    type
+    thc
+    cbd
+    image
+    id
+  }
+}
+`;
+
+class Budz extends Component{
+
+  heelo = () =>{
+    return(
+      <h1>Hello</h1>
+    )
+  }
+
+  render(){
+    const {data: {loading, budList}} = this.props;
+    if(loading){
+      return null;
+    }
+
+      const listOfBudz = budList.map(bud =>(
+          <div className="row">
+              <div className="card col-6" id="carta">
+                <img className="card-img-top budImage" ></img>
+                <div className="card-body">
+                  <h5 className="card-title lead">Strain Name: <span className="budStrain">{bud.strain}</span></h5>
+                  <p className="card-text lead">Type: <span className="budType">{bud.type}</span></p>
+                  <p className="card-text lead">THC%: <span className="budTHC">{bud.thc}</span></p>
+                  <p className="card-text lead">CBD%: <span className="budCBD">{bud.cbd}</span></p>
+                </div>
+              </div>
+          </div>
+      ))
+
+    return(
+      this.heelo,
+      listOfBudz
+    )
+  }
 }
 
-export default Budz;
+export default graphql(BudQuery)(Budz);
