9ac552ce88689f5ada58be53507378b545368373
Jason Solorio <jasonsoloriom@gmail.com>
2018-11-04 00:43:34 -0600

    New model for dabs, created the type for dabs and added in crud functionality for the dabs

--- server/index.js
+++ server/index.js
@@ -5,13 +5,23 @@ mongoose.connect('mongodb://localhost/jsbudz');
 
 //Budz model
 //bud is the collection name
+
 const Bud = mongoose.model('bud', {
-  strain: String,
-  type: String,
-  thc: String,
-  cbd: String,
-  image: String
+  budStrain: String,
+  budType: String,
+  budTHC: String,
+  budCBD: String,
+  budImage: String
 })
+
+const Dab = mongoose.model('dab', {
+  dabStrain: String,
+  dabType: String,
+  dabTHC: String,
+  dabCBD: String,
+  dabImage: String
+})
+
 
 //Below is the schema
 //type Query meaning View/Read
@@ -21,43 +31,73 @@ const Bud = mongoose.model('bud', {
 const typeDefs = `
   type Query{
     hello(name: String): String!
-    budList: [budType]
+    budList: [budType],
+    dabList: [dabType]
   }
 
   type budType{
     id: ID!
-    strain: String!
-    type: String!
-    thc: String!
-    cbd: String!
-    image: String
+    budStrain: String!
+    budType: String!
+    budTHC: String!
+    budCBD: String!
+    budImage: String!
   }
 
+  type dabType{
+    id: ID!
+    dabStrain: String!
+    dabType: String!
+    dabTHC: String!
+    dabCBD: String!
+    dabImage: String!
+  }
+
+
+
   type Mutation{
-    createBud(strain: String!, type: String!, thc: String!, cbd: String!, image: String): budType
-    updateBud(id: ID!, strain: String!): Boolean
+    createBud(budStrain: String!, budType: String!, budTHC: String!, budCBD: String!, budImage: String!): budType
+    updateBud(id: ID!, budImage: String!): Boolean
     removeBud(id: ID!): Boolean
+
+    createDab(dabStrain: String!, dabType: String!, dabTHC: String!, dabCBD: String!, dabImage: String!): dabType
+    updateDab(id: ID!, dabImage: String!): Boolean
+    removeDab(id: ID!): Boolean
   }
 `
 
 const resolvers = {
   Query: {
     hello: (_, { name}) => `Hello ${name || 'World!'}`,
-    budList: () => Bud.find()
+    budList: () => Bud.find(),
+    dabList: () => Dab.find()
   },
   //After specifying in Mutation we need to write it below twice, once in the createBudz and once in the const you made
   Mutation: {
-    createBud: async (_, { strain, type, thc, cbd, image}) =>{
-      const budz = new Bud({ strain, type, thc, cbd, image});
+    createBud: async (_, { budStrain, budType, budTHC, budCBD, budImage}) =>{
+      const budz = new Bud({ budStrain, budType, budTHC, budCBD, budImage});
       await budz.save();
       return budz;
     },
-    updateBud: async (_, {id, strain}) =>{
-      await Bud.findByIdAndUpdate(id, {strain});
+    updateBud: async (_, {id, budImage}) =>{
+      await Bud.findByIdAndUpdate(id, {Image});
       return true;
     },
     removeBud: async (_, { id }) =>{
       await Bud.findByIdAndRemove(id);
+      return true;
+    },
+    createDab: async (_, {  dabStrain,  dabType,  dabTHC,  dabCBD,  dabImage}) =>{
+      const dabz = new Dab({  dabStrain,  dabType,  dabTHC,  dabCBD,  dabImage});
+      await dabz.save();
+      return dabz;
+    },
+    updateDab: async (_, {id, image}) =>{
+      await Dab.findByIdAndUpdate(id, { dabImage});
+      return true;
+    },
+    removeDab: async (_, { id }) =>{
+      await Dab.findByIdAndRemove(id);
       return true;
     }
   }
