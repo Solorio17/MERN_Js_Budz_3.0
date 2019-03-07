cb8751497094646ca8d2469d77d7f62fcdbac5d5
Solorio17 <jasonsoloriom@gmail.com>
2019-03-06 21:48:53 -0700

    Moved Location

--- app/src/components/drinkz.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Drinkz = () =>{
-  return(
-    <h1>Hello from Drinkz</h1>
-  )
-}
-
-export default Drinkz;
--- app/src/components/ediblez.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Ediblez = () =>{
-  return(
-    <h1>Hello from Ediblez</h1>
-  )
-}
-
-export default Ediblez;
